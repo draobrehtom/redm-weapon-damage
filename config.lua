23b0750a4e74044da82bd63c0dcfa7cb4234b201
Drao <draobrehtom@gmail.com>
2024-07-01 11:45:37 +0200

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,73 @@
+Config = {}
+
+Config.PvPDamageOnly = false
+
+Config.WeaponDamage = {
+    -- Melee
+    {Name = 'WEAPON_UNARMED', Damage = 1.0, EnableCritical = true},
+    {Name = 'WEAPON_MELEE_CLEAVER', Damage = 1.0, EnableCritical = true},
+    {Name = 'WEAPON_MELEE_HAMMER', Damage = 1.0, EnableCritical = true},
+    {Name = 'WEAPON_MELEE_HATCHET', Damage = 1.0, EnableCritical = true},
+    {Name = 'WEAPON_MELEE_HATCHET_HUNTER', Damage = 1.0, EnableCritical = true},
+    {Name = 'WEAPON_MELEE_KNIFE', Damage = 1.0, EnableCritical = true},
+    {Name = 'WEAPON_MELEE_KNIFE_HORROR', Damage = 1.0, EnableCritical = true},
+    {Name = 'WEAPON_MELEE_KNIFE_JAWBONE', Damage = 1.0, EnableCritical = true},
+    {Name = 'WEAPON_MELEE_KNIFE_RUSTIC', Damage = 1.0, EnableCritical = true},
+    {Name = 'WEAPON_MELEE_KNIFE_TRADER', Damage = 1.0, EnableCritical = true},
+    {Name = 'WEAPON_MELEE_MACHETE', Damage = 1.0, EnableCritical = true},
+    {Name = 'WEAPON_MELEE_MACHETE_COLLECTOR', Damage = 1.0, EnableCritical = true},
+    {Name = 'WEAPON_MELEE_MACHETE_HORROR', Damage = 1.0, EnableCritical = true},
+
+
+    -- Revolver
+    {Name = 'WEAPON_REVOLVER_CATTLEMAN', Damage = 1.0, EnableCritical = true},
+    {Name = 'WEAPON_REVOLVER_CATTLEMAN_MEXICAN', Damage = 1.0, EnableCritical = true},
+    {Name = 'WEAPON_REVOLVER_DOUBLEACTION', Damage = 1.0, EnableCritical = true},
+    {Name = 'WEAPON_REVOLVER_DOUBLEACTION_GAMBLER', Damage = 1.0, EnableCritical = true},
+    {Name = 'WEAPON_REVOLVER_LEMAT', Damage = 1.0, EnableCritical = true},
+    {Name = 'WEAPON_REVOLVER_NAVY', Damage = 1.0, EnableCritical = true},
+    {Name = 'WEAPON_REVOLVER_NAVY_CROSSOVER', Damage = 1.0, EnableCritical = true},
+    {Name = 'WEAPON_REVOLVER_SCHOFIELD', Damage = 1.0, EnableCritical = true},
+
+    -- Pistols
+    {Name = 'WEAPON_PISTOL_M1899', Damage = 1.0, EnableCritical = true},
+    {Name = 'WEAPON_PISTOL_MAUSER', Damage = 1.0, EnableCritical = true},
+    {Name = 'WEAPON_PISTOL_SEMIAUTO', Damage = 1.0, EnableCritical = true},
+    {Name = 'WEAPON_PISTOL_VOLCANIC', Damage = 1.0, EnableCritical = true},
+
+    -- Snipers
+    {Name = 'WEAPON_SNIPERRIFLE_CARCANO', Damage = 1.0, EnableCritical = true},
+    {Name = 'WEAPON_SNIPERRIFLE_ROLLINGBLOCK', Damage = 1.0, EnableCritical = true},
+
+    -- Rifle
+    {Name = 'WEAPON_RIFLE_BOLTACTION', Damage = 1.0, EnableCritical = true},
+    {Name = 'WEAPON_RIFLE_ELEPHANT', Damage = 1.0, EnableCritical = true},
+    {Name = 'WEAPON_RIFLE_SPRINGFIELD', Damage = 1.0, EnableCritical = true},
+    {Name = 'WEAPON_RIFLE_VARMINT', Damage = 1.0, EnableCritical = true},
+
+    -- Repeater
+    {Name = 'WEAPON_REPEATER_CARBINE', Damage = 1.0, EnableCritical = true},
+    {Name = 'WEAPON_REPEATER_EVANS', Damage = 1.0, EnableCritical = true},
+    {Name = 'WEAPON_REPEATER_HENRY', Damage = 1.0, EnableCritical = true},
+    {Name = 'WEAPON_REPEATER_WINCHESTER', Damage = 1.0, EnableCritical = true},
+
+    -- Thrown
+    {Name = 'WEAPON_THROWN_DYNAMITE', Damage = 1.0, EnableCritical = true},
+    -- {Name = 'WEAPON_THROWN_MOLOTOV', Damage = 1.0, EnableCritical = true}, -- Doesn't have affect
+
+    {Name = 'WEAPON_THROWN_POISONBOTTLE', Damage = 1.0, EnableCritical = true},
+    {Name = 'WEAPON_THROWN_THROWING_KNIVES', Damage = 1.0, EnableCritical = true},
+    {Name = 'WEAPON_THROWN_TOMAHAWK', Damage = 1.0, EnableCritical = true},
+    {Name = 'WEAPON_THROWN_TOMAHAWK_ANCIENT', Damage = 1.0, EnableCritical = true},
+
+    -- Shotgun
+    {Name = 'WEAPON_SHOTGUN_DOUBLEBARREL', Damage = 1.0, EnableCritical = true},
+    {Name = 'WEAPON_SHOTGUN_PUMP', Damage = 1.0, EnableCritical = true},
+    {Name = 'WEAPON_SHOTGUN_REPEATING', Damage = 1.0, EnableCritical = true},
+    {Name = 'WEAPON_SHOTGUN_SAWEDOFF', Damage = 1.0, EnableCritical = true},
+    {Name = 'WEAPON_SHOTGUN_SEMIAUTO', Damage = 1.0, EnableCritical = true},
+
+    -- Bows
+    {Name = 'WEAPON_BOW', Damage = 1.0, EnableCritical = true},
+    {Name = 'WEAPON_BOW_IMPROVED', Damage = 1.0, EnableCritical = true},
+}
