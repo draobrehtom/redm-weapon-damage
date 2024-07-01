888f9299cf96a5e77cfd36b2f833d49d510210e2
Drao <draobrehtom@gmail.com>
2024-07-01 11:45:21 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version "cerulean"
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+games {"rdr3"}
+lua54 "yes"
+
+client_scripts {
+    'config.lua',
+    'client.lua',
+}
